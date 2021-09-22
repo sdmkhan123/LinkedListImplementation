090c5a34b0b84abb72ee874aa6f10daffcc1caeb
sdmkhan123 <sdmkhan126410@gmail.com>
2021-09-22 15:13:40 +0530

    Welcome to LinkedList implementation

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LinkedListImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****************************************************************");
+            Console.WriteLine("Welcome to LinkedList implementation World!\n");
+            Console.WriteLine("****************************************************************\n");
+        }
+    }
+}
